dd5c408eeb5705cb57884ef3f4b1c79e3af37b70
Gina <1ginahong@gmail.com>
2019-08-11 23:49:15 +0900

    add styled-components to config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
         //     }
         // },
         'gatsby-plugin-sass',
+        'gatsby-plugin-styled-components',
         {
             resolve: 'gatsby-source-filesystem',
             options: {
